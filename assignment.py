bfced7e4098339f8031b48e0195e7e95fa1aae58
srilatha kadari <srilathayadav.k@gmail.com>
2023-02-07 01:22:44 +0530

    First Assignment

--- assignment.py
+++ assignment.py
@@ -1,1 +1,71 @@
+# this prints hello world
 print('hello world');
+
+
+# this is the addition program
+def add(a, b):
+    return a + b
+
+
+# this is the subtraction program
+def sub(a, b):
+    return a - b
+
+
+# this is the multiplication program
+def multi(a, b):
+    return a * b
+
+
+# this is the division program
+def div(a, b):
+    return a / b
+
+
+# calling all the functions and printing the results
+print("addition : ", add(2, 3))
+print("subtraction : ", sub(4, 9))
+print("multiplication : ", multi(8, 9))
+print("division : ", div(9, 3))
+
+# xyz are variables
+x = 2
+y = 3.0
+z = 5j
+
+# printing the type of variable
+print(type(x))
+print(type(y))
+print(type(z))
+
+# xyz are the variables
+x = 4
+y = 44.0
+z = 8j
+
+# convert from  int into float
+print(x)
+print(type(x))
+
+# convert from float into int
+print(y)
+print(type(y))
+
+# convert from complex into float
+print(z)
+print(type(z))
+
+# x will be 2 and y will be 6 and z will be 11
+x = int(2)
+y = int(6.8)
+z = int(11)
+
+# x will be 6 and y will be 7and z will be 122.1
+x = float(6)
+y = float(7)
+z = float(122.1)
+
+# x will be 1 and y will be 8i and z will be 1.13
+x = str(1)
+y = str(8j)
+z = str(1.13)
